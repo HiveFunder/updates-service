861249a63e2bb8ff31b10be9fac3e59a3c1b5ccf
Jordan Holmes <jordanstholmes@gmail.com>
2018-11-02 19:40:39 -0700

    Make test component to test merging

---
+++ client/components/noData.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NoData() {
+  return <h1>We cannot load Updates at this time. Please check back later</h1>
+}
+
+export default NoData;
--- client/index.jsx
+++ client/index.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import axios from 'axios';
 import App from './components/app';
+import NoData from './components/noData';
 
 const PROJECT_ID = 5;
 
 document.addEventListener('DOMContentLoaded', () => {
   axios.get(`http://localhost:3000/${PROJECT_ID}/updates`).then(response => {
     ReactDom.render(<App updates={response.data} />, document.getElementById('root'));
-  });
+  })
+  .catch(err => {
+    console.error(err);
+    ReactDom.render(<NoData />, document.getElementById('root'));
+  })
 });
